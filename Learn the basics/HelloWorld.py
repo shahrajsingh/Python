fb2653567a504af94524d044b3f389f9988609ad
Shahraj Singh <shahraj@outlook.com>
2020-05-26 08:57:21 +0530

    first comit

---
+++ Learn the basics/HelloWorld.py
@@ -0,0 +1,21 @@
+# the Symbol # is used for inline comments(single line)
+# A computer program is the method by which we talk with computer
+# and instruct it to carry out some task so first let's teach computer to talk
+# below is the hello world program
+
+# To make computer talk print() syntax is used in python
+# Strings are placed between ""(double quotes)
+print("hello World")
+
+# or '' (single quotes)
+print('hello world')
+
+# "" are useful as we could print strings containing single quotes('') e.g:-
+print("Let's Go")
+# the above mentioned print would have not been possible if we would have used ('')
+
+
+# the followint code would work in python 2
+# python 2 allows print syntax as below remove # in line 15 to see it work
+#print "hello world"
+# above mentioned code would result int error in python 3
